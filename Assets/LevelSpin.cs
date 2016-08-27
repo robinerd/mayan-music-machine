1a6301c181b0d46d6cd7879dbea0e7541f6fd59b
Robinerd <robinerd@gmail.com>
2016-08-27 16:31:50 +0200

    Spinning the level!

--- Assets/LevelSpin.cs
+++ Assets/LevelSpin.cs
@@ -3,13 +3,42 @@ using System.Collections;
 
 public class LevelSpin : MonoBehaviour {
 
+    public float beatsPerMinute;
+    public float beatsPerRevolution;
+
+    float secondsPerRevolution;
+
 	// Use this for initialization
 	void Start () {
-	
+        float secondsPerBeat = 60.0f / beatsPerMinute;
+        secondsPerRevolution = secondsPerBeat * beatsPerRevolution;
+        GetComponent<Rigidbody2D>().angularVelocity = 360.0f / secondsPerRevolution;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        float seconds = Time.timeSinceLevelLoad;
+
+        // Wrap the absolute music time into the revolution time interval.
+        while(seconds > secondsPerRevolution)
+        {
+            seconds -= secondsPerRevolution;
+        }
+        float targetRevolutionProgress = seconds / secondsPerRevolution;
+        float targetAngle = 360.0f * targetRevolutionProgress;
+
+        float angle = GetComponent<Rigidbody2D>().rotation;
+        while (angle < 0.0f)
+            angle += 360.0f;
+        while (angle > 360.0f)
+            angle -= 360.0f;
+
+        Debug.Log(targetAngle + " , " + angle);
+
+        if (angle < targetAngle - 0.2f)
+            GetComponent<Rigidbody2D>().AddTorque(5);
+        else if(angle > targetAngle + 0.2f)
+            GetComponent<Rigidbody2D>().AddTorque(-5);
+
+    }
 }
